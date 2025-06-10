1ffa0ebc2086c0bf2a04e42f050fb85793d6ca5d
Rajat <rajatgarg0903@gmail.com>
2025-06-10 18:34:13 +0530

    my

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,23 @@ app.get("/allMssg/:chatId", auth, allMessages); // api/message/:id
 app.use(express.json());
 app.use(express.static('build'));
 
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get('/{*any}', (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+
 const server = app.listen(5000, () => {
   console.log("Running");
 });
