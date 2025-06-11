65a681a25e64d0d790c31104770fac2c6ca379d3
Rajat <rajatgarg0903@gmail.com>
2025-06-11 11:04:12 +0530

    cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 const connectDb = async() =>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/chat');
-    console.log("connected");
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }catch(error){
+        console.error(`Error: ${error.message}`.red.bold);
+        process.exit(1);
+
+    }
 }
 
 module.exports = connectDb;
